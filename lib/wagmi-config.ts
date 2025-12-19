1ff69a9cb841397249f28f16043cae0a7ea36af3
ditsy <Ardiansyah.ditsy@gmail.com>
2025-12-19 20:16:08 +0700

    main

--- lib/wagmi-config.ts
+++ lib/wagmi-config.ts
@@ -1,7 +1,6 @@
 import { http, createConfig } from 'wagmi'
 import { base } from 'wagmi/chains'
 import { farcasterMiniApp as miniAppConnector } from '@farcaster/miniapp-wagmi-connector'
-import { reown } from '@reown/appkit-adapter-wagmi'
 
 export const config = createConfig({
   chains: [base],
@@ -9,16 +8,8 @@ export const config = createConfig({
     [base.id]: http(),
   },
   connectors: [
-    miniAppConnector(),
-    reown({
-      projectId: process.env.NEXT_PUBLIC_REOWN_PROJECT_ID!,
-      metadata: {
-        name: 'Farcaster Reputation Passport',
-        description: 'A reputation-based NFT passport for the Farcaster ecosystem',
-        url: process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000',
-        icons: [`${process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'}/icon.png`]
-      }
-    })
+    miniAppConnector()
+    // Reown connector can be added after proper package installation
   ]
 })
 
